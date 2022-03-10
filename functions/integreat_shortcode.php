a3093eb097d3c99f9cc12d98c401a526741d0a10
Johannes <stock@integreat-app.de>
2022-03-10 16:53:46 +0100

    Fixes issues with url

--- functions/integreat_shortcode.php
+++ functions/integreat_shortcode.php
@@ -18,7 +18,7 @@ function integreat_render_design_big($options) {
     ?>
         <div id="integreat_plugin_big" class="integreat_plugin integreat_plugin_big">
             <div class="integreat_plugin_column integreat_plugin_column_1_3">
-                <img src="<?php if($options['image_big'] != ''){echo $options['image_big'];} else {?> https://integreat-app.de/wp-content/uploads/2020/11/T7A5938-scaled.jpg <?php } ?>">
+                <img src="<?php if($options['integreat_alternative_image'] != ''){echo $options['integreat_alternative_image'];} else {?> https://integreat-app.de/wp-content/uploads/2020/11/T7A5938-scaled.jpg <?php } ?>">
             </div>
             <div class="integreat_plugin_column integreat_plugin_column_2_3">
                 <h2 class="mb-md"><b><?php if($options['language'] != 'on') { ?> Kennen Sie schon die Integreat-App? <?php } else { ?> Kennst du schon die Integreat-App? <?php } ?></b></h2>
@@ -40,7 +40,7 @@ function integreat_render_design_big($options) {
 
 function integreat_render_design_bg_image($options) {
     ?>
-        <div <?php if($options['image_bg'] != '') {?> style="background-image: url('<?php echo $options['image_bg'] ?>')" <?php } ?>id="integreat_plugin_bg_image" class="integreat_plugin integreat_plugin_layout integreat_plugin_bg_image">
+        <div <?php if($options['integreat_alternative_image'] != '') {?> style="background-image: url('<?php echo $options['integreat_alternative_image'] ?>')" <?php } ?>id="integreat_plugin_bg_image" class="integreat_plugin integreat_plugin_layout integreat_plugin_bg_image">
             <h2 class="mb-md"><b><?php if($options['language'] != 'on') { ?> Kennen Sie schon die Integreat-App? <?php } else { ?> Kennst du schon die Integreat-App?<?php } ?></b></h2>
             <p class="mb-lg"><?php if($options['language'] != 'on') { ?> Hier finden Sie alle wichtigen Informationen aus Ihrer Kommune, <?php } else { ?> Hier findest Du alle wichtigen Informationen aus deiner Kommune, <?php } ?>
             z.B. Anmeldung im Bürgerservice, Infos rund um den Arztbesuch,
@@ -59,7 +59,7 @@ function integreat_render_design_bg_image($options) {
 function integreat_render_design_small($options) {
     ?>
     <div id="integreat_plugin_small" class="integreat_plugin integreat_plugin_small">
-        <img class="mb-md" src="<?php if($options['image_small'] != ''){ echo $options['image_small']; }else {?> https://integreat-app.de/wp-content/uploads/2020/11/T7A5938-scaled.jpg <?php } ?>">
+        <img class="mb-md" src="<?php if($options['integreat_alternative_image'] != ''){ echo $options['integreat_alternative_image']; }else {?> https://integreat-app.de/wp-content/uploads/2020/11/T7A5938-scaled.jpg <?php } ?>">
         <h2 class="mb-md"><b><?php if($options['language'] != 'on') { ?> Kennen Sie schon die Integreat-App? <?php } else { ?> Kennst du schon die Integreat-App?<?php } ?></b></h2>
         <p class="mb-lg"><?php if($options['language'] != 'on') { ?> Hier finden Sie alle wichtigen Informationen aus Ihrer Kommune, <?php } else { ?> Hier findest Du alle wichtigen Informationen aus deiner Kommune, <?php } ?>
             z.B. Anmeldung im Bürgerservice, Infos rund um den Arztbesuch,
