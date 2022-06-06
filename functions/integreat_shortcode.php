b34b21b779a59beee2a0245d451c69aff2e83cae
Timo Ludwig <ludwig+secure@integreat-app.de>
2022-06-06 20:01:13 +0200

    Change escape function for city code, fixes #44

--- functions/integreat_shortcode.php
+++ functions/integreat_shortcode.php
@@ -25,7 +25,7 @@ function integreat_render_design_big($options) {
                 <p class="mb-lg"><?php echo esc_html($options['paragraph']) ?></p>
                 <label class="mb-sm"><b><?php echo esc_html($options['notification']) ?></b></label>
                 <div class="integreat_plugin_search_bar">
-                    <form action="https://integreat.app/<?php echo esc_url(strtolower(get_option('integreat_plugin_options')['city'])) ?>/de/search" method="get"> <!-- Sprachauswahl backend -->
+                    <form action="https://integreat.app/<?php echo esc_html(strtolower(get_option('integreat_plugin_options')['city'])) ?>/de/search" method="get"> <!-- Sprachauswahl backend -->
                         <input class="integreat_plugin_search" id="integreat_plugin_search" name="query" placeholder=<?php if (strlen($options['term']) > 2) { echo esc_attr($options['term']); } else {echo esc_attr('Integrationskurse');}  ?>>
                         <input class="integreat_plugin_submit" id="integreat_plugin_submit" type="submit" formtarget="_blank" value="<?php esc_attr_e( 'Search' ); ?>">
                     </form>
@@ -38,14 +38,14 @@ function integreat_render_design_big($options) {
 function integreat_render_design_bg_image($options) {
     ?>
         <div <?php if($options['integreat_alternative_image'] != '') {?> style="background-image: url('<?php echo esc_url($options['integreat_alternative_image']) ?>')" <?php } ?>id="integreat_plugin_bg_image" class="integreat_plugin integreat_plugin_layout integreat_plugin_bg_image">
-            <a class="integreat_plugin_icon_small" target="_blank" href="https://www.integreat.app/<?php echo esc_url(strtolower(get_option('integreat_plugin_options')['city'])) ?>">
+            <a class="integreat_plugin_icon_small" target="_blank" href="https://www.integreat.app/<?php echo esc_html(strtolower(get_option('integreat_plugin_options')['city'])) ?>">
                 <img class="mb-md" src="https://integreat.app/app-logo.png">
             </a>
             <h2 class="mb-md"><b><?php echo esc_html($options['headline']) ?></b></h2>
             <p class="mb-lg"><?php echo esc_html($options['paragraph']) ?></p>
             <label class="mb-sm"><b><?php echo esc_html($options['notification']) ?></b></label>
             <div class="integreat_plugin_search_bar">
-                <form action="https://integreat.app/<?php echo esc_url(strtolower(get_option('integreat_plugin_options')['city'])) ?>/de/search" method="get"> <!-- Sprachauswahl backend -->
+                <form action="https://integreat.app/<?php echo esc_html(strtolower(get_option('integreat_plugin_options')['city'])) ?>/de/search" method="get"> <!-- Sprachauswahl backend -->
                     <input class="integreat_plugin_search" id="integreat_plugin_search" value="<?php if (strlen($options['term']) > 2) { echo esc_attr($options['term']); } else {echo esc_attr('Integrationskurse');} ?>" name="query" placeholder="<?php if (strlen($options['term']) > 2) { echo esc_attr($options['term']); } else {echo esc_attr('Integrationskurse');} ?>">
                     <input class="integreat_plugin_submit" id="integreat_plugin_submit" type="submit" formtarget="_blank" value="<?php esc_attr_e( 'Search' ); ?>">
                 </form>
@@ -57,7 +57,7 @@ function integreat_render_design_bg_image($options) {
 function integreat_render_design_small($options) {
     ?>
     <div id="integreat_plugin_small" class="integreat_plugin integreat_plugin_small">
-        <a class="integreat_plugin_icon_small" target="_blank" href="https://www.integreat.app/<?php echo esc_url(strtolower(get_option('integreat_plugin_options')['city'])) ?>">
+        <a class="integreat_plugin_icon_small" target="_blank" href="https://www.integreat.app/<?php echo esc_html(strtolower(get_option('integreat_plugin_options')['city'])) ?>">
             <img class="mb-md" src="https://integreat.app/app-logo.png">
         </a>
         <img class="mb-md" src="<?php if($options['integreat_alternative_image'] != ''){ echo esc_url($options['integreat_alternative_image']); }else {?> https://integreat-app.de/wp-content/uploads/2020/11/T7A5938-scaled.jpg <?php } ?>">
@@ -65,7 +65,7 @@ function integreat_render_design_small($options) {
         <p class="mb-lg"><?php echo esc_html($options['paragraph']) ?></p>
         <label class="mb-sm"><b><?php echo esc_html($options['notification']) ?></b></label>
         <div class="integreat_plugin_search_bar">
-            <form action="https://integreat.app/<?php echo esc_url(strtolower(get_option('integreat_plugin_options')['city'])) ?>/de/search" method="get"> <!-- Sprachauswahl backend -->
+            <form action="https://integreat.app/<?php echo esc_html(strtolower(get_option('integreat_plugin_options')['city'])) ?>/de/search" method="get"> <!-- Sprachauswahl backend -->
                 <input class="integreat_plugin_search" id="integreat_plugin_search" name="query" value="<?php if (strlen($options['term']) > 2) { echo esc_attr($options['term']); } else {echo esc_attr('Integrationskurse');} ?>" placeholder="<?php if (strlen($options['term']) > 2) { echo esc_attr($options['term']); } else {echo esc_attr('Integrationskurse');} ?>">
                 <input class="integreat_plugin_submit" id="integreat_plugin_submit" type="submit" formtarget="_blank" value="<?php esc_attr_e( 'Search' ); ?>">
             </form>
@@ -77,7 +77,7 @@ function integreat_render_design_small($options) {
 function integreat_render_design_search_widget($options) {
     ?>
     <div id="integreat_plugin_search_widget" class="integreat_plugin_search_widget">
-        <a target="_blank" href="https://www.integreat.app/<?php echo esc_url(strtolower(get_option('integreat_plugin_options')['city'])) ?>">
+        <a target="_blank" href="https://www.integreat.app/<?php echo esc_html(strtolower(get_option('integreat_plugin_options')['city'])) ?>">
             <img class="mb-md" src="https://integreat.app/app-logo.png">
         </a>
         <label class="mb-sm"><b><?php echo esc_html($options['notification']) ?></b></label>
